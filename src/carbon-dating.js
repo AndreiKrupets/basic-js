b1e89360972377bd1fd307a7fbf5a5802f4c972e
Andrei <aikey@mail.ru>
2020-09-29 04:42:44 +0300

    feat: carbon-dating Done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,9 +6,9 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity !== 'string') {return false};
   let sample = parseFloat(sampleActivity);
-  if (sample < 0 || sample > 15 ) {return false};
+  if (sample <= 0 || sample > 15 || isNaN(sample)) {return false};
   let k = 0.693 / HALF_LIFE_PERIOD;
-  let age = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k;
+  let age = (Math.log(MODERN_ACTIVITY / sample)) / k;
   age = Math.ceil(age);
   return age;
 };

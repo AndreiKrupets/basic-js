55ff2c2556c7adaab492f231e0673c57cbe6f57b
Andrei <aikey@mail.ru>
2020-09-30 02:14:19 +0300

    feat: dream team. 3 fails

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members !== 'object') {return false}
+  if (members.length === 0) {return null}
+  let result = '';
+  let membersOrder = members.sort();
+  for (let name of members) {
+    if (typeof name === 'string') {
+      name = name.trim(); 
+      result += name[0].toUpperCase();}
+    
+}
+return result;
 };

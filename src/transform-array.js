e910ce7261bd89dc9f40873d696b6997a9d1a3ca
Andrei <aikey@mail.ru>
2020-09-30 23:46:17 +0300

    feat: transform array+-

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  for (let member of arr) {
+    if (typeof member === 'string') {
+      switch (member) {
+       case "--discard-next":
+        arr = arr.splice(arr.indexOf(member), 2)
+        break;
+       case "--discard-prev":
+        arr = arr.splice(arr.indexOf(member)-1, 2)
+        break;
+       case "--double-next":
+        member = arr[arr.indexOf(member) + 1];
+        break;
+       case "--double-prev":
+         member = arr[arr.indexOf(member) - 1];
+         break; }
+    }
+  }
+  return arr;
 };

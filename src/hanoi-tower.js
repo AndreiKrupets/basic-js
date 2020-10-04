e88df4b6901339312089ad0ac700fb86f22f3826
Andrei <aikey@mail.ru>
2020-10-04 23:07:23 +0300

    feat: hanoi towers++

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,6 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turns = 2 ** disksNumber - 1;
-    let seconds = Math.floor(turns * turnsSpeed / 3600)
+    let seconds = Math.floor(turns * 3600 / turnsSpeed);
     return {turns, seconds}
 };

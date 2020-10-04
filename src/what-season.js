3d56a3d64f34a4f8eef15a82ce4bedb5ed21fd80
Andrei <aikey@mail.ru>
2020-10-04 14:31:53 +0300

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  if(!date) {return "Unable to determine the time of year!"}
   
+  const month = date.getMonth();
+  switch(month) {
+      case month < 2:
+          return 'winter';
+          break;
+      case month < 5:
+          return 'spring';
+          break;
+      case month < 8:
+          return 'summer';
+          break;
+      case month < 11:
+          return 'autumn';
+          break;    
+      case month < 12:
+          return 'winter';
+          break;
+  } 
 };

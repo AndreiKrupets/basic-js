ff00ebe17e335a1c5c50e94fbb39a23ed872289b
Andrei <aikey@mail.ru>
2020-10-05 17:42:06 +0300

    fix: what season. all tests passed

--- src/what-season.js
+++ src/what-season.js
@@ -1,27 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if(!date) {return "Unable to determine the time of year!"};
-  if (typeof date.getMonth !== typeof Date().getMonth){
-      throw new Error ('')
-  }
-  
+    if (date == null) return "Unable to determine the time of year!";
+    if (isNaN(date)) {throw new Error('');
+};
+
   const month = date.getMonth();
   switch(month) {
-      case month < 2:
-          return 'winter';
-          break;
-      case month < 5:
-          return 'spring';
-          break;
-      case month < 8:
-          return 'summer';
-          break;
-      case month < 11:
-          return 'autumn';
-          break;    
-      case month < 12:
-          return 'winter';
-          break;
-  } 
+    case 0:
+        return 'winter';
+    case 1:
+        return 'winter';
+    case 11:
+        return 'winter';
+    case 2:
+        return 'spring';
+    case 3:
+        return 'spring';
+    case 4:
+        return 'spring';
+    case 5:
+        return 'summer';
+    case 6:
+        return 'summer';
+    case 7:
+        return 'summer';
+    case 8:
+        return 'autumn';
+    case 9:
+        return 'autumn';
+    case 10:
+        return 'autumn';
+
+  }
 };
